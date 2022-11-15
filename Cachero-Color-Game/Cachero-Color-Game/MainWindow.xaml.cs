34d7b23c797c5ff27d57f4318791c0c09276a0d3
Julette Peque <22-0200c@sgen.edu.ph>
2022-11-15 20:14:32 +0800

    Changed Dice data type to Label

--- Cachero-Color-Game/Cachero-Color-Game/MainWindow.xaml.cs
+++ Cachero-Color-Game/Cachero-Color-Game/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace Cachero_Color_Game
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Rectangle[] gameColorDice = new Rectangle[] { }; 
+        private Label[] gameColorDice = new Label[] { }; 
+        private Grid gameDiceGrid = new Grid();
 
         public MainWindow()
         {
@@ -29,6 +30,7 @@ namespace Cachero_Color_Game
         }
         private void initDices() 
         {
+            initDiceGrid();
             int h = 5;
 
             List<Color> listOfColors = new List<Color>();
@@ -39,23 +41,49 @@ namespace Cachero_Color_Game
             listOfColors.Add(Color.FromRgb(255, 140, 0)); //Color of Orange
             listOfColors.Add(Color.FromRgb(255, 0, 255)); // Color of Purple
 
-            gameColorDice = new Rectangle[3];
+            gameColorDice = new Label[3];
 
             for (int i = 0; i < gameColorDice.Length; i++) 
             {
-                Rectangle dice = new Rectangle();
+                Label dice = new Label();
                 dice.VerticalAlignment = VerticalAlignment.Top;
                 dice.HorizontalAlignment = HorizontalAlignment.Left;
-                dice.Width = 60;
-                dice.Height = 60;
+                dice.VerticalContentAlignment= VerticalAlignment.Center;
+                dice.HorizontalContentAlignment= HorizontalAlignment.Center;
+                dice.Width = 80;
+                dice.Height = 80;
                 dice.Margin = new Thickness(h,10,0,0);
-                dice.Fill = new SolidColorBrush(listOfColors.ElementAt(5));
+                dice.BorderBrush = new SolidColorBrush(Colors.Black);
+                dice.BorderThickness = new Thickness(1,1,1,1);
+                switch (i) 
+                {
+                    case 0:
+                        dice.Content = "Dice 1";
+                        break;
+                    case 1:
+                        dice.Content = "Dice 2";
+                        break;
+                    case 2:
+                        dice.Content = "Dice 3";
+                        break;
+                }
                 gameColorDice[i] = dice;
-                mainGrid.Children.Add(gameColorDice[i]);
+                gameDiceGrid.Children.Add(gameColorDice[i]);
 
-                h += 5 + (int)dice.Width;
+                h += 10 + (int)dice.Width;
             }
 
+        }
+
+        private void initDiceGrid() 
+        {
+            gameDiceGrid = new Grid();
+            gameDiceGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            gameDiceGrid.VerticalAlignment = VerticalAlignment.Top;
+            gameDiceGrid.Width = 300;
+            gameDiceGrid.Height = 90;
+            gameDiceGrid.Margin = new Thickness(350,200,0,0);
+            mainGrid.Children.Add(gameDiceGrid);
         }
     }
 }
